28391e65242c74047084ebd28fd9b2ddf5489a3c
BourgeoisDirk <51524225+BourgeoisDirk@users.noreply.github.com>
2025-10-31 22:36:20 +0100

    Enhance Downsize Folder.bat with quality parameter
    Added quality encoding input for video conversion.

--- Downsize Folder.bat
+++ Downsize Folder.bat
@@ -1,11 +1,13 @@
  @ECHO OFF
- SET exe=%cd%\ffmpeg.exe
+ SET exe=%cd%\..\ffmpeg.exe
 
  SET /p "source=Enter Source Directory: "
  SET target=%source%\Converted
-
  MKDIR "%target%" 2> NULL
  ECHO Outputting to "%target%"
+ ECHO.
+ ECHO We need a Quality Encoding Value: Scale is 0 to 51, where 0 is lossless, 23 is the default, and 51 is worst quality possible
+ SET /p "quality=Enter the Quality Value: "
  PAUSE
 
  PUSHD "%source%" 
@@ -17,7 +19,7 @@
    ECHO File already Converted "%%F"
   ) ELSE (
    ECHO Converting "%%F"
-   "%exe%" -loglevel error -stats -nostdin -y -i "%%F" -c:s copy -map 0 -crf 24 "%target%\%%F"
+   "%exe%" -loglevel error -stats -nostdin -y -i "%%F" -c:s copy -map 0 -crf %quality% "%target%\%%F"
   )
  )
  ENDLOCAL
