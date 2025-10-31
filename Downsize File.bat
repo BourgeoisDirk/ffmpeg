c4ccd25b391168387fb745ec7113381ddb31afd5
BourgeoisDirk <51524225+BourgeoisDirk@users.noreply.github.com>
2025-10-31 22:39:29 +0100

    Fix ffmpeg path in Downsize File.bat

--- Downsize File.bat
+++ Downsize File.bat
@@ -4,6 +4,7 @@ set /p "source=Enter Source Path: "
 set /p "target=Enter Target Path: "
 set /p "quality=Enter Quality Compression (0 to 51, lower is better): "
 
-"..\ffmpeg.exe" -nostdin -y -i %source% -c:s copy -map 0 -crf %quality% %target%
+"ffmpeg.exe" -nostdin -y -i %source% -c:s copy -map 0 -crf %quality% %target%
+
 
 PAUSE  
